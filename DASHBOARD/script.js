06bb3d08cbd4fb8d1342f2f89d6db5b1f1b5237d
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-07-19 07:38:22 +0300

    Compute annual income

--- DASHBOARD/script.js
+++ DASHBOARD/script.js
@@ -1,24 +1,21 @@
 var sellingPrice = 45;
-var totalProduction
-var totalDailyProduction = a + b + c + d
+var totalDailyProduction = a + b + c + d;
 
 function sum() {
-    a = Number(document.calculator.a.value);
-    b = Number(document.calculator.b.value);
-    c = Number(document.calculator.c.value);
-    d = Number(document.calculator.d.value);
-    totalDailyProduction= a + b + c + d ;
-    document.calculator.total.value = totalDailyProduction;
-    document.calculator.daily.value = totalDailyProduction * sellingPrice
+  a = Number(document.calculator.a.value);
+  b = Number(document.calculator.b.value);
+  c = Number(document.calculator.c.value);
+  d = Number(document.calculator.d.value);
+  totalDailyProduction = a + b + c + d;
+  document.calculator.total.value = totalDailyProduction;
+  document.calculator.daily.value = totalDailyProduction * sellingPrice;
 }
-
-
-
 
 function weekly() {
-    
-    document.calculator2.week.value= totalDailyProduction * sellingPrice * 7  
+  document.calculator2.week.value = totalDailyProduction * sellingPrice * 7;
 }
 
-
-
+function yearly() {
+  document.getElementById("year").value =
+    totalDailyProduction * sellingPrice * 365;
+}
